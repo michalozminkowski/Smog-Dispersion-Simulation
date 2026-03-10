71f88904615b2a95515a72a408b09151653ddce7
michalozminkowski <michalozminkowski673@gmail.com>
2026-03-10 14:29:57 +0100

    Refactor: FastAPI Lifespan dla natychmiastowego ładowania strony

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 import os
 import os.path
-
-os.environ["DOCKER_ENV"] = "True"
-
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from app import smog_sim
 
-app = FastAPI()
+os.environ["DOCKER_ENV"] = "True"
 
-@app.get("/")
-def read_root():
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
     gif_filename = "smog_symulacja.gif"
 
     if not os.path.exists(gif_filename):
         smog_sim.main()
 
-    return FileResponse(gif_filename, media_type="image/gif")
+    yield
+
+app = FastAPI(lifespan=lifespan)
+
+@app.get("/")
+def read_root():
+    return FileResponse("smog_symulacja.gif", media_type="image/gif")
